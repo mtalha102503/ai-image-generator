f259cf583b4c7ae711bcbe64255bb25a2c1b437f
mtalha102503 <realonlinejobs56@gmail.com>
2025-06-20 09:20:27 +0500

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
- res.setHeader("Access-Control-Allow-Origin", "https://www.geniartify.online");
-  res.setHeader("Access-Control-Allow-Methods", "POST");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+res.setHeader("Access-Control-Allow-Origin", "https://www.geniartify.online");
+res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
 if (req.method === "OPTIONS") {
     return res.status(200).end();
